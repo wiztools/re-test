b636d85cb1137e663b3ee8dfa19db637240242be
Subhash Chandran <subwiz@gmail.com>
2009-11-24 00:09:39 +0000

    options added

--- src/main/java/org/wiztools/util/retest/REFrame.java
+++ src/main/java/org/wiztools/util/retest/REFrame.java
@@ -2,12 +2,14 @@ package org.wiztools.util.retest;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -32,6 +34,11 @@ public class REFrame extends JFrame {
     private JButton jb_match = new JButton("  Match  ");
     private JButton jb_clear = new JButton("Clear");
 
+    private JCheckBox jcb_caseinsensitive = new JCheckBox("Case insensitive");
+    private JCheckBox jcb_dotall = new JCheckBox(". matches newline too");
+    private JCheckBox jcb_multiline = new JCheckBox("Multi-line");
+    // private JCheckBox jcb_comments = new JCheckBox("Ignore whitespaces & comments");
+
     private final REFrame me;
 
     public REFrame(String title){
@@ -48,6 +55,7 @@ public class REFrame extends JFrame {
 
         this.pack();
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
@@ -78,13 +86,30 @@ public class REFrame extends JFrame {
                 }
                 else{
                     try{
-                        Pattern p = Pattern.compile(re);
+                        // Always specify this:
+                        int option = Pattern.CANON_EQ;
+
+                        if(jcb_caseinsensitive.isSelected()){
+                            option = option | Pattern.CASE_INSENSITIVE;
+                            option = option | Pattern.UNICODE_CASE;
+                        }
+                        if(jcb_multiline.isSelected()){
+                            option = option | Pattern.MULTILINE;
+                        }
+                        if(jcb_dotall.isSelected()){
+                            option = option | Pattern.DOTALL;
+                        }
+                        
+                        Pattern p = Pattern.compile(re, option);
                         Matcher m = p.matcher(input);
                         if(m.find()){
                             jta_out.append("<<Match found!>>\n\n");
                             for(int i=1; i<=m.groupCount(); i++){
                                 jta_out.append("<<Group: " + i + ">>\n");
-                                jta_out.append("\t" + m.group(i) + "\n");
+                                String[] arr = m.group(i).split("\n");
+                                for(String s: arr){
+                                    jta_out.append("\t" + s + "\n");
+                                }
                             }
                         }
                         else{
@@ -106,13 +131,25 @@ public class REFrame extends JFrame {
 
         // North
         JPanel jp_north = new JPanel();
-        jp_north.setLayout(new BorderLayout(BORDER_LAYOUT_SPACING,
-                BORDER_LAYOUT_SPACING));
+        jp_north.setLayout(new GridLayout(2, 1));
+
+        JPanel jp_north_1 = new JPanel();
+        jp_north_1.setLayout(new BorderLayout());
         JLabel jl = new JLabel("RE: ");
         jl.setDisplayedMnemonic('r');
         jl.setLabelFor(jtf_re);
-        jp_north.add(jl, BorderLayout.WEST);
-        jp_north.add(jtf_re, BorderLayout.CENTER);
+        jp_north_1.add(jl, BorderLayout.WEST);
+        jp_north_1.add(jtf_re, BorderLayout.CENTER);
+        jp_north.add(jp_north_1);
+
+        JPanel jp_north_2 = new JPanel();
+        jp_north_2.setLayout(new FlowLayout(FlowLayout.LEFT));
+        jp_north_2.add(jcb_caseinsensitive);
+        jp_north_2.add(jcb_multiline);
+        jp_north_2.add(jcb_dotall);
+        // jp_north_2.add(jcb_comments);
+        
+        jp_north.add(jp_north_2);
 
         jp.add(jp_north, BorderLayout.NORTH);
 
