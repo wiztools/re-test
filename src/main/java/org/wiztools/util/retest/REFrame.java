a971d6c8e3a915d03ed161fa08e27fcea5ed934b
Subhash Chandran <subwiz@gmail.com>
2009-12-23 11:12:20 +0000

    real-time computation enabled: removed the match and clear buttons

--- src/main/java/org/wiztools/util/retest/REFrame.java
+++ src/main/java/org/wiztools/util/retest/REFrame.java
@@ -2,23 +2,26 @@ package org.wiztools.util.retest;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
-import javax.swing.JButton;
+import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 /**
  *
@@ -28,22 +31,19 @@ public class REFrame extends JFrame {
 
     private static final int BORDER_LAYOUT_SPACING = 5;
 
-    private JTextField jtf_re = new JTextField(40);
-    private JTextArea jta_in = new JTextArea(10, 5);
-    private JTextArea jta_out = new JTextArea(10, 5);
-    private JButton jb_match = new JButton("  Match  ");
-    private JButton jb_clear = new JButton("Clear");
+    private final JTextField jtf_re = new JTextField(40);
+    private final JTextArea jta_in = new JTextArea(10, 5);
+    private final JTextArea jta_out = new JTextArea(10, 5);
 
-    private JCheckBox jcb_caseinsensitive = new JCheckBox("Case insensitive");
-    private JCheckBox jcb_dotall = new JCheckBox(". matches newline too");
-    private JCheckBox jcb_multiline = new JCheckBox("Multi-line");
+    private final JCheckBox jcb_caseinsensitive = new JCheckBox("Case insensitive");
+    private final JCheckBox jcb_dotall = new JCheckBox(". matches newline too");
+    private final JCheckBox jcb_multiline = new JCheckBox("Multi-line");
     // private JCheckBox jcb_comments = new JCheckBox("Ignore whitespaces & comments");
 
-    private final REFrame me;
+    private final JLabel jl_status = new JLabel("WizTools.org RE Tester");
 
     public REFrame(String title){
         super(title);
-        me = this;
         
         init();
 
@@ -60,68 +60,38 @@ public class REFrame extends JFrame {
     }
 
     private void init(){
+        final DocumentListener dl = new DocumentListener() {
+            public void insertUpdate(DocumentEvent e) {
+                match();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                match();
+            }
+
+            public void changedUpdate(DocumentEvent e) {
+                match();
+            }
+        };
+
+        jtf_re.getDocument().addDocumentListener(dl);
+        jta_in.getDocument().addDocumentListener(dl);
+
+        final ItemListener il = new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                match();
+            }
+        };
+
+        jcb_caseinsensitive.addItemListener(il);
+        jcb_dotall.addItemListener(il);
+        jcb_multiline.addItemListener(il);
+
         jtf_re.setToolTipText("Regular Expression");
         jta_in.setToolTipText("Input text");
         jta_out.setEditable(false);
         jta_out.setToolTipText("Result");
-
-        jb_clear.setMnemonic('c');
-        jb_clear.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                jta_out.setText("");
-            }
-        });
-
-        jb_match.setMnemonic('m');
-        jb_match.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Clear before doing anything:
-                jta_out.setText("");
-
-                final String re = jtf_re.getText();
-                final String input = jta_in.getText();
-                if(re.trim().equals("")){
-                    JOptionPane.showMessageDialog(me, "No RE given!");
-                    jtf_re.requestFocus();
-                }
-                else{
-                    try{
-                        // Always specify this:
-                        int option = Pattern.CANON_EQ;
-
-                        if(jcb_caseinsensitive.isSelected()){
-                            option = option | Pattern.CASE_INSENSITIVE;
-                            option = option | Pattern.UNICODE_CASE;
-                        }
-                        if(jcb_multiline.isSelected()){
-                            option = option | Pattern.MULTILINE;
-                        }
-                        if(jcb_dotall.isSelected()){
-                            option = option | Pattern.DOTALL;
-                        }
-                        
-                        Pattern p = Pattern.compile(re, option);
-                        Matcher m = p.matcher(input);
-                        if(m.find()){
-                            jta_out.append("<<Match found!>>\n\n");
-                            for(int i=1; i<=m.groupCount(); i++){
-                                jta_out.append("<<Group: " + i + ">>\n");
-                                String[] arr = m.group(i).split("\n");
-                                for(String s: arr){
-                                    jta_out.append("\t" + s + "\n");
-                                }
-                            }
-                        }
-                        else{
-                            jta_out.setText("<<Does not match!>>");
-                        }
-                    }
-                    catch(PatternSyntaxException ex){
-                        JOptionPane.showMessageDialog(me, ex.getMessage());
-                    }
-                }
-            }
-        });
+        jl_status.setFont(new Font(Font.DIALOG, Font.PLAIN, 12)); // 10 is the font size
     }
 
     private JPanel initInput(){
@@ -157,15 +127,6 @@ public class REFrame extends JFrame {
         JScrollPane jsp = new JScrollPane(jta_in);
 
         jp.add(jsp, BorderLayout.CENTER);
-
-        // South
-        JPanel jp_south = new JPanel();
-        jp_south.setLayout(new FlowLayout(FlowLayout.CENTER));
-        jp_south.add(jb_match);
-        jp_south.add(jb_clear);
-
-        jp.add(jp_south, BorderLayout.SOUTH);
-
         return jp;
     }
 
@@ -175,6 +136,67 @@ public class REFrame extends JFrame {
                 BORDER_LAYOUT_SPACING));
         JScrollPane jsp = new JScrollPane(jta_out);
         jp.add(jsp, BorderLayout.CENTER);
+
+        JPanel jp_south = new JPanel();
+        jp_south.setLayout(new GridLayout(1, 1));
+        jp_south.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+        jp_south.add(jl_status);
+
+        jp.add(jp_south, BorderLayout.SOUTH);
         return jp;
+    }
+
+    private void setStatus(final String msg){
+        jl_status.setText(msg);
+        jl_status.setToolTipText(msg);
+    }
+
+    // Business logic:
+    private void match(){
+        // Clear before doing anything:
+        jta_out.setText("");
+
+        final String re = jtf_re.getText();
+        final String input = jta_in.getText();
+        if(re.trim().equals("")){
+            setStatus("No RE given!");
+            jtf_re.requestFocus();
+        }
+        else{
+            try{
+                // Always specify this:
+                int option = Pattern.CANON_EQ;
+
+                if(jcb_caseinsensitive.isSelected()){
+                    option = option | Pattern.CASE_INSENSITIVE;
+                    option = option | Pattern.UNICODE_CASE;
+                }
+                if(jcb_multiline.isSelected()){
+                    option = option | Pattern.MULTILINE;
+                }
+                if(jcb_dotall.isSelected()){
+                    option = option | Pattern.DOTALL;
+                }
+
+                Pattern p = Pattern.compile(re, option);
+                Matcher m = p.matcher(input);
+                if(m.find()){
+                    jta_out.append("<<Match found!>>\n\n");
+                    for(int i=1; i<=m.groupCount(); i++){
+                        jta_out.append("<<Group: " + i + ">>\n");
+                        String[] arr = m.group(i).split("\n");
+                        for(String s: arr){
+                            jta_out.append("\t" + s + "\n");
+                        }
+                    }
+                }
+                else{
+                    jta_out.setText("<<Does not match!>>");
+                }
+            }
+            catch(PatternSyntaxException ex){
+                setStatus(ex.getMessage());
+            }
+        }
     }
 }
