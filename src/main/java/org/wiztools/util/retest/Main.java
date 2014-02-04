57a08c4e8d0f93bd87d5c149c99b7a7bb0c22673
Subhash Chandran <subwiz@gmail.com>
2014-02-04 22:15:10 +0530

    Removed the space between Regular and Expression

--- src/main/java/org/wiztools/util/retest/Main.java
+++ src/main/java/org/wiztools/util/retest/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] arg){
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new REFrame("WizTools.org Regular Expression Tester");
+                new REFrame("WizTools.org RegularExpression Tester");
             }
         });
     }
